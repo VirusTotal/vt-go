a56c8ca90b662662bc2726b1165b4d88803aec28
Francisco Santos <fsantos@virustotal.com>
2019-09-16 22:27:26 +0200

    Feature: Add VT Monitor file uploads

--- filescan.go
+++ filescan.go
@@ -43,15 +43,14 @@ type FileScanner struct {
 	cli *Client
 }
 
-// Scan sends a file to VirusTotal for scanning. The file content is read from
+// VtFileUpload sends a file to VirusTotal. The file content is read from
 // the r io.Reader and sent to VirusTotal with the provided file name which can
 // be left blank. The function also sends a float32 through the progress channel
 // indicating the percentage of the file that has been already uploaded. The
 // progress channel can be nil if the caller is not interested in receiving
-// upload progress updates. An analysis object is returned as soon as the file
+// upload progress updates. The received object is returned as soon as the file
 // is uploaded.
-func (s *FileScanner) Scan(r io.Reader, filename string, progress chan<- float32) (*Object, error) {
-
+func (s *FileScanner) VtFileUpload(r io.Reader, filename string, endpoint string, params map[string]string, progress chan<- float32) (*Object, error) {
 	var uploadURL *url.URL
 	var payloadSize int64
 
@@ -69,20 +68,25 @@ func (s *FileScanner) Scan(r io.Reader, filename string, progress chan<- float32
 		return nil, err
 	}
 
+	if params != nil {
+		for key, value := range params {
+			w.WriteField(key, value)
+		}
+	}
 	w.Close()
 
 	if payloadSize > payloadMaxSize {
 		// Payload is bigger than supported by AppEngine in a POST request,
 		// let's ask for an upload URL.
 		var u string
-		if _, err := s.cli.GetData(URL("files/upload_url"), &u); err != nil {
+		if _, err := s.cli.GetData(URL(endpoint+"/upload_url"), &u); err != nil {
 			return nil, err
 		}
 		if uploadURL, err = url.Parse(u); err != nil {
 			return nil, err
 		}
 	} else {
-		uploadURL = URL("files")
+		uploadURL = URL(endpoint)
 	}
 
 	pr := &progressReader{
@@ -111,8 +115,27 @@ func (s *FileScanner) Scan(r io.Reader, filename string, progress chan<- float32
 	return analysis, nil
 }
 
+// Scan sends a file to VirusTotal for scanning.
+func (s *FileScanner) Scan(r io.Reader, filename string, progress chan<- float32) (*Object, error) {
+	return s.VtFileUpload(r, filename, "files", nil, progress)
+}
+
 // ScanFile sends a file to VirusTotal for scanning. This function is similar to
 // Scan but it receive an *os.File instead of a io.Reader and a file name.
 func (s *FileScanner) ScanFile(f *os.File, progress chan<- float32) (*Object, error) {
 	return s.Scan(f, f.Name(), progress)
+}
+
+// MonitorUploadFilename uploads a file to your VT Monitor account using a destination path
+func (s *FileScanner) MonitorUploadFilename(r io.Reader, monitorPath string, progress chan<- float32) (*Object, error) {
+	params := map[string]string{"path": monitorPath}
+	// noname is needed so gae accept the file
+	return s.VtFileUpload(r, "noname", "monitor/items", params, progress)
+}
+
+// MonitorUploadItem uploads a file to your VT Monitor account using a destination monitor_id
+func (s *FileScanner) MonitorUploadItem(r io.Reader, monitorItem string, progress chan<- float32) (*Object, error) {
+	params := map[string]string{"item": monitorItem}
+	// noname is needed so gae accept the file
+	return s.VtFileUpload(r, "noname", "monitor/items", params, progress)
 }
