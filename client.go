fce0077b709b945ebf9f55868e431d604dcc3d7d
Jose Toro <josetoro@google.com>
2022-04-13 16:17:16 +0200

    feat(Client): Allow sending data in DELETE requests (#22)

--- client.go
+++ client.go
@@ -207,9 +207,21 @@ func (cli *Client) Patch(url *url.URL, req *Request, options ...RequestOption) (
 }
 
 // Delete sends a DELETE request to the specified API endpoint.
-func (cli *Client) Delete(url *url.URL, options ...RequestOption) (*Response, error) {
-	o := opts(options...)
-	httpResp, err := cli.sendRequest("DELETE", url, nil, o.headers)
+func (cli *Client) Delete(url *url.URL, req *Request, options ...RequestOption) (*Response, error) {
+	var b []byte
+	var err error
+	if req != nil {
+		b, err = json.Marshal(req)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// Default Content-Type header to application/json in DELETE requests.
+	defaultContentTypeOptions := append(
+		[]RequestOption{WithHeader("Content-Type", "application/json")},
+		options...)
+	o := opts(defaultContentTypeOptions...)
+	httpResp, err := cli.sendRequest("DELETE", url, bytes.NewReader(b), o.headers)
 	if err != nil {
 		return nil, err
 	}
@@ -238,6 +250,14 @@ func (cli *Client) PostData(url *url.URL, data interface{}, options ...RequestOp
 	req := &Request{}
 	req.Data = data
 	return cli.Post(url, req, options...)
+}
+
+// DeleteData sends a DELETE request to the specified API endpoint. The data argument
+// is JSON-encoded and wrapped as {'data': <JSON-encoded data> }.
+func (cli *Client) DeleteData(url *url.URL, data interface{}, options ...RequestOption) (*Response, error) {
+	req := &Request{}
+	req.Data = data
+	return cli.Delete(url, req, options...)
 }
 
 // PostObject adds an Object to a collection. The specified URL must point to
